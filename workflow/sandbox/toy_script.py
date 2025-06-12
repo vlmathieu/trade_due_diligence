0c8821595b56bd9b5ad17b5f6336b04f034f30ff
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-06-12 11:55:07 +0200

    More fun more game.

---
+++ workflow/sandbox/toy_script.py
@@ -0,0 +1,23 @@
+import polars as pl
+import polars.selectors as cs
+import pickle
+import re
+import numpy as np
+import networkx as nx
+from scipy.stats import kurtosis
+from scipy.stats import skew
+import pandas as pd
+import itertools
+
+# Parameters
+year_start = 2020
+year_stop = 2024
+cmdCode = [4403, 4407, 4408, 4812]
+flowCode = ['M', 'X']
+excluded_iso = ['XX', '_X', '\\d']
+
+# Load data
+uncomtrade_data = pl.read_parquet('/Users/valentinmathieu/Desktop/wd/trade_due_diligence/resources/public/uncomtrade_data.parquet.gzip')
+
+# Exploring data
+uncomtrade_data.columns
