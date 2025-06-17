0c7e8b4bcc5c051b745ad01b1670015dab7b17b2
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-06-17 17:03:33 +0200

    Creating new rule and script to create binary variables in dataframe (work in progress).

---
+++ workflow/rules/group_bin.smk
@@ -0,0 +1,15 @@
+rule group_bin:
+    input:
+        'results/input/input_data.parquet.gzip',
+    output:
+        'results/intermediary/data_group_bin.parquet.gzip'
+    params:
+        policies   = config['policy'],
+        risky      = config['risky']['countries']
+    log:
+        'workflow/logs/group_bin.log'
+    threads: 1
+    conda:
+        '../envs/polars.yaml'
+    script:
+        '../scripts/group_bin.py'
---
+++ workflow/scripts/group_bin.py
@@ -0,0 +1,50 @@
+from snakemake.script import snakemake
+import polars as pl
+
+# Load data
+input_data = pl.read_parquet(snakemake.input[0])
+
+# Get due diligence policies and correspondong countries from parameters
+pol_params = snakemake.params['policies']
+policies = list(pol_params.keys())
+policy_countries = [_ for p in policies for _ in pol_params[p]['countries']]
+
+# Get list of risky countries
+risky = snakemake.params['risky']
+
+# Produce binary variables for policies
+for _ in policies:
+    input_data = (
+        input_data
+        .with_columns(pl.col('period').cast(pl.Int32))
+        .with_columns(
+                pl.when(pl.col('period') >= pol_params[_]['year'])
+                .then(1)
+                .otherwise(0)
+                .alias(f'{_}')
+            )
+    )
+
+# Produce binary variables for country groups
+for _ in ['reporter', 'partner']:
+    input_data = (
+        input_data
+        .with_columns(
+            pl.when(pl.col(f'{_}_desc').is_in(risky))
+            .then(1)
+            .otherwise(0)
+            .alias(f'{_}_risky')
+        )
+        .with_columns(
+            pl.when(pl.col(f'{_}_desc').is_in(policy_countries))
+            .then(1)
+            .otherwise(0)
+            .alias(f'{_}_policy')
+        )
+        .with_columns(
+            pl.when(~pl.col(f'{_}_desc').is_in(policy_countries+risky))
+            .then(1)
+            .otherwise(0)
+            .alias(f'{_}_non_risky')
+        )
+    )
