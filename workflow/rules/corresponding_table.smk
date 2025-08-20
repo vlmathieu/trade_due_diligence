aeaa279dd7a8604487f459f13d69f876c87d253e
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-08-20 18:04:43 +0200

    Update rule and code.

--- workflow/rules/corresponding_table.smk
+++ workflow/rules/corresponding_table.smk
@@ -1,11 +1,6 @@
 rule corresponding_table:
     output:
         'resources/inhouse/corresponding_table.csv'
-    params:
-        cmdCode         = config['uncomtrade']['cmdCode'],
-        cmdDesc         = config['uncomtrade']['cmdDesc'],
-        fao_items       = config['fao']['items'],
-        fao_item_codes  = config['fao']['item_codes']
     log:
         'workflow/logs/corresponding_table.log'
     threads: 1
--- workflow/scripts/corresponding_table.py
+++ workflow/scripts/corresponding_table.py
@@ -9,14 +9,33 @@ logging.basicConfig(filename=snakemake.log[0],
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 # Build corresponding table
-corr_table = pl.from_dict(
-    {
-        "cmdCode": snakemake.params['cmdCode'],
-        "cmdDesc": snakemake.params['cmdDesc'],
-        "faoCode": snakemake.params['fao_item_codes'],
-        "faoDesc": snakemake.params['fao_items']
-    }
+
+corr_table = pl.from_dicts(
+    [
+
+        {
+            'cmdCode': 4403,
+            'cmdDesc': 'Wood in the rough, whether or not stripped of bark or sapwood, or roughly squared.',
+            'faoCode': 1861,
+            'faoDesc': 'Roundwood'
+        },
+
+        {
+            'cmdCode': 4407,
+            'cmdDesc': 'Wood sawn or chipped lengthwise, sliced or peeled, whether or not planed, sanded or end-jointed, of a thickness exceeding 6 mm.',
+            'faoCode': 1872,
+            'faoDesc': 'Sawnwood'
+        },
+
+        {
+            'cmdCode': 4412,
+            'cmdDesc': 'Plywood, veneered panels and similar laminated wood.',
+            'faoCode': 1640,
+            'faoDesc': 'Plywood and LVL' # LVL = Laminated veneer lumber
+        }
+    ]
 )
+
 logging.info(f"Corresponding table built:\n {corr_table}")
 
 # Save corresponding table
